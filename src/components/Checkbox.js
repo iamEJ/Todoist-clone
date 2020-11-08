41f3991969ddd5cfc4673ed1c9d3a4ba80b1218f
iamEJ <jukneviciute185@gmail.com>
2020-11-08 17:29:35 +0200

    add Tasks and Checkbox components

---
+++ src/components/Checkbox.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { firebase } from "../firebase";
+
+export const Checkbox = ({ id }) => {
+  const archivedTask = () => {
+    firebase.firestore().collection("tasks").doc(id).update({ archive: true });
+  };
+  return (
+    <div
+      className="checkbox-holder"
+      data-testid="checkbox-action"
+      onClick={() => archivedTask()}
+    >
+      <span className="checkbox" />
+    </div>
+  );
+};
