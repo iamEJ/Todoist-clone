40db615a94d0af9d7a5f7563aca981e509ff1bb8
iamEJ <jukneviciute185@gmail.com>
2020-12-09 15:42:04 +0200

    add onClick to sidebar components

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -8,35 +8,61 @@ import { Projects } from "../Projects";
 import { AddProject } from "../AddProject";
 
 export function Sidebar() {
-  const { selectedProject } = useSelectedProjectValue();
+  const { setSelectedProject } = useSelectedProjectValue();
   const [active, setActive] = useState("inbox");
   const [showProjects, setShowProjects] = useState(true);
 
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li data-testid="inbox" className="inbox">
+        <li
+          data-testid="inbox"
+          className={active === "inbox" ? "active" : undefined}
+          onClick={() => {
+            setActive("inbox");
+            setSelectedProject("INBOX");
+          }}
+        >
           <span>
             <InboxIcon />
           </span>
           <span>Inbox</span>
         </li>
-        <li data-testid="today" className="today">
+        <li
+          data-testid="today"
+          className={active === "today" ? "active" : undefined}
+          onClick={() => {
+            setActive("today");
+            setSelectedProject("TODAY");
+          }}
+        >
           <span>
             <CalendarTodayIcon />
           </span>
           <span>Today</span>
         </li>
-        <li data-testid="next_7" className="next_7">
+        <li
+          data-testid="next_7"
+          className={active === "next_7" ? "active" : undefined}
+          onClick={() => {
+            setActive("next_7");
+            setSelectedProject("NEXT_7");
+          }}
+        >
           <span>
             <DateRangeIcon />
           </span>
           <span>Next 7 days</span>
         </li>
       </ul>
-      <div className="sidebar__middle">
+      <div
+        className="sidebar__middle"
+        onClick={() => setShowProjects(!showProjects)}
+      >
         <span>
-          <KeyboardArrowDownIcon />
+          <KeyboardArrowDownIcon
+            className={!showProjects ? "hidden-projects" : undefined}
+          />
         </span>
         <h2>Projects</h2>
       </div>
