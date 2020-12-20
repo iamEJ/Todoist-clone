dbc573df797a83bc0eef5a53da38dce69a256113
iamEJ <jukneviciute185@gmail.com>
2020-12-20 19:09:21 +0200

    test <Checkbox/> component

---
+++ src/__tests__/Checkbox.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import { Checkbox } from "../components/Checkbox";
+
+beforeEach(cleanup); // clean the DOM
+
+describe("<Checkbox />", () => {
+  describe("Success", () => {
+    it("Renders task checkbox", () => {
+      const { queryByTestId } = render(<Checkbox id="1" />);
+      expect(queryByTestId("checkbox-action")).toBeTruthy();
+    });
+    it("Renders task checkbox and accepts a onClick ", () => {
+      const { queryByTestId } = render(<Checkbox id="1" />);
+      expect(queryByTestId("checkbox-action")).toBeTruthy();
+      fireEvent.click(queryByTestId("checkbox-action"));
+    });
+    it("Renders task checkbox and accepts a onKeyDown ", () => {
+      const { queryByTestId } = render(<Checkbox id="1" />);
+      expect(queryByTestId("checkbox-action")).toBeTruthy();
+      fireEvent.keyDown(queryByTestId("checkbox-action"));
+    });
+  });
+});
